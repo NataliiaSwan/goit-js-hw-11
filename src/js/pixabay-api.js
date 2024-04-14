bbb649f4c8943e5779607233888bb9abb5006f32
NataliiaSwan <148957684+NataliiaSwan@users.noreply.github.com>
2024-04-14 18:12:19 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,5 @@
 export function doFetch(query) {
+    const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '43290538-553335dd3499f06be9a5135a9';
   const params = new URLSearchParams({
     key: API_KEY,
@@ -7,7 +8,7 @@ export function doFetch(query) {
     orientation: 'horizontal',
     safesearch: true,
   });
-  return fetch(`https://pixabay.com/api?${params}`).then(response => {
+  return fetch(`${BASE_URL}?${params}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
